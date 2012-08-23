0d17bac93f43aa8f0f83510334fc8303777840e0
niujin <niujin@gmail.com>
2012-08-23 17:19:23 +0800

    aa

--- src/info/realjin/newsintime/activity/CollectionListActivity.java
+++ src/info/realjin/newsintime/activity/CollectionListActivity.java
@@ -2,98 +2,119 @@ package info.realjin.newsintime.activity;
 
 import info.realjin.newsintime.R;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
-import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 /*
  * http://www.cnblogs.com/allin/archive/2010/05/11/1732200.html
+ * http://appfulcrum.com/2010/09/12/listview-example-3-simple-multiple-selection-checkboxes/
+ * http://gqdy365.iteye.com/blog/992340
  */
 
-public class CollectionListActivity extends ListActivity {
-	private List<Map<String, Object>> mData;
-
-	public void onCreate(Bundle savedInstanceState) {
+public class CollectionListActivity extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.collectionlist);
 
-//		setContentView(R.layout.collections);
-		//
-		// LinearLayout llCollections = (LinearLayout)
-		// findViewById(R.id.llcollections);
-		//
-		// ListView lvCollections = new ListView(this);
-		// llCollections.addView(lvCollections);
+		ListView list = (ListView) findViewById(R.id.lv);
+		MyAdapter adapter = new MyAdapter(this);
+		list.setAdapter(adapter);
+		list.setItemsCanFocus(false);
+		list.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 
-	}
-
-	final class ViewHolder {
-		// public ImageView img;
-		public TextView text;
-		public Button btEdit;
-	}
-
-	class CollectionListAdapter extends BaseAdapter {
-
-		private LayoutInflater li;
-
-		public CollectionListAdapter(Context context) {
-			this.li = LayoutInflater.from(context);
-		}
-
-		public int getCount() {
-			// TODO Auto-generated method stub
-			return mData.size();
-		}
-
-		public Object getItem(int arg0) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		public long getItemId(int arg0) {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-
-		public View getView(int position, View convertView, ViewGroup parent) {
-
-			ViewHolder holder = null;
-			if (convertView == null) {
-
-				holder = new ViewHolder();
-
-				// convertView = li.inflate(R.layout.vlist2, null);
-				// holder.img = (ImageView) convertView.findViewById(R.id.img);
-				// holder.title = (TextView)
-				// convertView.findViewById(R.id.title);
-				// holder.btEdit = (Button)
-				// convertView.findViewById(R.id.view_btn);
-
-			} else {
-
-				holder = (ViewHolder) convertView.getTag();
+		list.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				ViewHolder vHollder = (ViewHolder) view.getTag();
+				// 在每次获取点击的item时将对于的checkbox状态改变，同时修改map的值。
+				vHollder.cBox.toggle();
+				MyAdapter.isSelected.put(position, vHollder.cBox.isChecked());
 			}
-
-			holder.text.setText((String) mData.get(position).get("text"));
-
-			holder.btEdit.setOnClickListener(new View.OnClickListener() {
-
-				public void onClick(View v) {
-					// showInfo();
-				}
-			});
-
-			return convertView;
-		}
+		});
 
 	}
+
+}
+
+class MyAdapter extends BaseAdapter {
+	private LayoutInflater mInflater;
+	private List<Map<String, Object>> mData;
+	public static Map<Integer, Boolean> isSelected;
+
+	public MyAdapter(Context context) {
+		mInflater = LayoutInflater.from(context);
+		init();
+	}
+
+	// 初始化
+	private void init() {
+		mData = new ArrayList<Map<String, Object>>();
+		for (int i = 0; i < 5; i++) {
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("img", R.drawable.ic_launcher);
+			map.put("title", "第" + (i + 1) + "行的标题");
+			mData.add(map);
+		}
+		// 这儿定义isSelected这个map是记录每个listitem的状态，初始状态全部为false。
+		isSelected = new HashMap<Integer, Boolean>();
+		for (int i = 0; i < mData.size(); i++) {
+			isSelected.put(i, false);
+		}
+	}
+
+	public int getCount() {
+		return mData.size();
+	}
+
+	public Object getItem(int position) {
+		return null;
+	}
+
+	public long getItemId(int position) {
+		return 0;
+	}
+
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ViewHolder holder = null;
+		// convertView为null的时候初始化convertView。
+		if (convertView == null) {
+			holder = new ViewHolder();
+			convertView = mInflater.inflate(R.layout.collectionlist_listview,
+					null);
+			holder.img = (ImageView) convertView.findViewById(R.id.img);
+			holder.title = (TextView) convertView.findViewById(R.id.title);
+			holder.cBox = (CheckBox) convertView.findViewById(R.id.cb);
+			convertView.setTag(holder);
+		} else {
+			holder = (ViewHolder) convertView.getTag();
+		}
+		holder.img.setBackgroundResource((Integer) mData.get(position).get(
+				"img"));
+		holder.title.setText(mData.get(position).get("title").toString());
+		holder.cBox.setChecked(isSelected.get(position));
+		return convertView;
+	}
+
+}
+
+final class ViewHolder {
+	public ImageView img;
+	public TextView title;
+	public CheckBox cBox;
 }
