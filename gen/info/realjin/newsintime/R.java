900f1224106fb196f3805c98260f620d4c20f305
niujin <niujin@gmail.com>
2012-08-23 12:01:27 +0800

    aa

--- gen/info/realjin/newsintime/R.java
+++ gen/info/realjin/newsintime/R.java
@@ -39,8 +39,11 @@ public final class R {
         public static final int hello=0x7f040000;
     }
     public static final class style {
-        public static final int Widget=0x7f050000;
-        public static final int Widget_ProgressBar=0x7f050001;
-        public static final int Widget_ProgressBar_RegularProgressBar=0x7f050002;
+        public static final int Widget=0x7f050003;
+        public static final int Widget_ProgressBar=0x7f050004;
+        public static final int Widget_ProgressBar_RegularProgressBar=0x7f050005;
+        public static final int Widget1=0x7f050000;
+        public static final int Widget1_ProgressBar=0x7f050001;
+        public static final int Widget1_ProgressBar_RegularProgressBar=0x7f050002;
     }
 }
