6b5b57a0326038ae3b360b7e87594b957b5963e4
niujin <niujin@gmail.com>
2012-08-23 17:35:47 +0800

    aa

--- src/info/realjin/newsintime/activity/CollectionListActivity.java
+++ src/info/realjin/newsintime/activity/CollectionListActivity.java
@@ -1,9 +1,9 @@
 package info.realjin.newsintime.activity;
 
+import info.realjin.newsintime.NewsInTimeApp;
 import info.realjin.newsintime.R;
+import info.realjin.newsintime.domain.Collection;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -32,8 +32,15 @@ public class CollectionListActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.collectionlist);
 
+		NewsInTimeApp app = (NewsInTimeApp) getApplication();
+
 		ListView list = (ListView) findViewById(R.id.lv);
-		MyAdapter adapter = new MyAdapter(this);
+		CollectionListAdapter adapter = new CollectionListAdapter(this);
+
+		// get data dynamically
+		List<Collection> collections = app.getData().getCollectionList();
+		adapter.setColls(collections);
+
 		list.setAdapter(adapter);
 		list.setItemsCanFocus(false);
 		list.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
@@ -41,10 +48,12 @@ public class CollectionListActivity extends Activity {
 		list.setOnItemClickListener(new OnItemClickListener() {
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
-				ViewHolder vHollder = (ViewHolder) view.getTag();
-				// 在每次获取点击的item时将对于的checkbox状态改变，同时修改map的值。
-				vHollder.cBox.toggle();
-				MyAdapter.isSelected.put(position, vHollder.cBox.isChecked());
+//				CollectionListViewHolder vHollder = (CollectionListViewHolder) view
+//						.getTag();
+//				// 在每次获取点击的item时将对于的checkbox状态改变，同时修改map的值。
+//				vHollder.cBox.toggle();
+//				CollectionListAdapter.isSelected.put(position,
+//						vHollder.cBox.isChecked());
 			}
 		});
 
@@ -52,34 +61,34 @@ public class CollectionListActivity extends Activity {
 
 }
 
-class MyAdapter extends BaseAdapter {
+class CollectionListAdapter extends BaseAdapter {
 	private LayoutInflater mInflater;
-	private List<Map<String, Object>> mData;
+	private List<Collection> colls;
 	public static Map<Integer, Boolean> isSelected;
 
-	public MyAdapter(Context context) {
+	public CollectionListAdapter(Context context) {
 		mInflater = LayoutInflater.from(context);
-		init();
+		// init();
 	}
 
 	// 初始化
 	private void init() {
-		mData = new ArrayList<Map<String, Object>>();
-		for (int i = 0; i < 5; i++) {
-			Map<String, Object> map = new HashMap<String, Object>();
-			map.put("img", R.drawable.ic_launcher);
-			map.put("title", "第" + (i + 1) + "行的标题");
-			mData.add(map);
-		}
+		// mData = new ArrayList<Map<String, Object>>();
+		// for (int i = 0; i < 5; i++) {
+		// Map<String, Object> map = new HashMap<String, Object>();
+		// map.put("img", R.drawable.ic_launcher);
+		// map.put("title", "第" + (i + 1) + "行的标题");
+		// mData.add(map);
+		// }
 		// 这儿定义isSelected这个map是记录每个listitem的状态，初始状态全部为false。
-		isSelected = new HashMap<Integer, Boolean>();
-		for (int i = 0; i < mData.size(); i++) {
-			isSelected.put(i, false);
-		}
+		// isSelected = new HashMap<Integer, Boolean>();
+		// for (int i = 0; i < mData.size(); i++) {
+		// isSelected.put(i, false);
+		// }
 	}
 
 	public int getCount() {
-		return mData.size();
+		return colls.size();
 	}
 
 	public Object getItem(int position) {
@@ -91,10 +100,10 @@ class MyAdapter extends BaseAdapter {
 	}
 
 	public View getView(int position, View convertView, ViewGroup parent) {
-		ViewHolder holder = null;
+		CollectionListViewHolder holder = null;
 		// convertView为null的时候初始化convertView。
 		if (convertView == null) {
-			holder = new ViewHolder();
+			holder = new CollectionListViewHolder();
 			convertView = mInflater.inflate(R.layout.collectionlist_listview,
 					null);
 			holder.img = (ImageView) convertView.findViewById(R.id.img);
@@ -102,18 +111,26 @@ class MyAdapter extends BaseAdapter {
 			holder.cBox = (CheckBox) convertView.findViewById(R.id.cb);
 			convertView.setTag(holder);
 		} else {
-			holder = (ViewHolder) convertView.getTag();
+			holder = (CollectionListViewHolder) convertView.getTag();
 		}
-		holder.img.setBackgroundResource((Integer) mData.get(position).get(
-				"img"));
-		holder.title.setText(mData.get(position).get("title").toString());
-		holder.cBox.setChecked(isSelected.get(position));
+		// holder.img.setBackgroundResource((Integer) colls.get(position).get(
+		// "img"));
+		holder.title.setText(colls.get(position).getName());
+		// holder.cBox.setChecked(isSelected.get(position));
 		return convertView;
+	}
+
+	public List<Collection> getColls() {
+		return colls;
+	}
+
+	public void setColls(List<Collection> colls) {
+		this.colls = colls;
 	}
 
 }
 
-final class ViewHolder {
+final class CollectionListViewHolder {
 	public ImageView img;
 	public TextView title;
 	public CheckBox cBox;
