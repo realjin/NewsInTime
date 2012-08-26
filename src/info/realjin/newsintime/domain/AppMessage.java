5751be397a241bb5260132a30c6f4a8284dea3b2
niujin <niujin@gmail.com>
2012-08-26 19:48:34 +0800

    aa

---
+++ src/info/realjin/newsintime/domain/AppMessage.java
@@ -0,0 +1,29 @@
+package info.realjin.newsintime.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AppMessage {
+	private Map<String, Object> msgMap;
+	public static final String MSG_CILACT_CIACT_ITEM = "CILACT_CIACT_ITEM";
+
+	public AppMessage() {
+		msgMap = new HashMap<String, Object>();
+	}
+
+	public void putMessage(String name, Object msg) {
+		msgMap.put(name, msg);
+	}
+
+	public Object getMessage(String name) {
+		return msgMap.get(name);
+	}
+
+	public Map<String, Object> getMsgMap() {
+		return msgMap;
+	}
+
+	public void setMsgMap(Map<String, Object> msgMap) {
+		this.msgMap = msgMap;
+	}
+}
